b7474fcf5dc5cb1f787489ecc677a1de8ce06ff1
杨介正 <hzyangjiezheng@corp.netease.com>
2020-04-15 09:56:06 +0800

    feat: add .babelrc

--- babel.config.js
+++ babel.config.js
@@ -3,18 +3,5 @@ module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
   plugins: [
     ['@babel/plugin-proposal-decorators', {'legacy': true}],
-    [
-      "module-resolver",
-      {
-          "root": ["./src"],
-          "alias": {
-              "@util": "./src/util",
-              "@component": "./src/component",
-              "@service": "./src/service",
-              "@redux": "./src/redux",
-              "@container": "./src/container"
-          }
-      }
-    ]
   ]
 };
